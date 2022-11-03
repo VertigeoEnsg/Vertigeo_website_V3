6adca398fef82e0b4d0a5f05aeeb95b53b0cd7c5
GITGT200 <tiazaragaetan@gmail.com>
2022-11-03 21:24:58 +0100

    defilement_V1 almost complete
    defilement_V1

---
+++ js/carousel.js
@@ -0,0 +1,25 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    nav:true,
+    items:4,
+    loop:true,
+    margin:10,
+    // autoplay:true,
+    // autoplayTimeout:2000,
+    // autoplayHoverPause:true,
+    responsive: {
+        0: { items: 1, nav: false },
+        600: { items: 2, nav: false },
+        1000: { items: 3, nav: false },
+        1200: { items: 4, nav: false },
+    }
+});
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
