b27f711c4531bcd5e65e54673368992b9a5a3143
GITGT200 <tiazaragaetan@gmail.com>
2023-01-07 11:03:02 +0100

    partenaire

--- js/carousel.js
+++ js/carousel.js
@@ -4,9 +4,9 @@ owl.owlCarousel({
     items:4,
     loop:true,
     margin:10,
-    // autoplay:true,
-    // autoplayTimeout:2000,
-    // autoplayHoverPause:true,
+     autoplay:true,
+     autoplayTimeout:2000,
+     autoplayHoverPause:true,
     responsive: {
         0: { items: 1, nav: false },
         700: { items: 2, nav: false },
